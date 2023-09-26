738acd82e3dc2cd0ebb90506fec5ca87c2c192ba
RoachieBoy <64902177+RoachieBoy@users.noreply.github.com>
2023-09-26 15:43:29 +0200

    Buttons for switching
    Changelog:
    - Made and imported button sprites for each base synth module
    - Created basic code for switching between the wave types at runtime

---
+++ M.A.G.I/Assets/Scripts/BasicSynthModules/SynthModule.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace BasicSynthModules
+{
+    public abstract class SynthModule : ScriptableObject
+    {
+        protected int SampleRate;
+        protected double Phase;
+
+        private void Awake()
+        {
+            SampleRate = AudioSettings.outputSampleRate;
+            
+            // Make sure the phase is reset when the synth module is created
+            Phase = 0;
+        }
+        
+        /// <summary>
+        /// Generate samples for the given data array, channels, frequency, and amplitude.
+        /// </summary>
+        /// <param name="data">audio data</param>
+        /// <param name="channels">channels to generate audio from</param>
+        /// <param name="frequency">the frequency the generated audio should have</param>
+        /// <param name="amplitude">the amplitude of the audio wave</param>
+        public abstract void GenerateSamples(float[] data, int channels, float frequency, float amplitude);
+    }
+}

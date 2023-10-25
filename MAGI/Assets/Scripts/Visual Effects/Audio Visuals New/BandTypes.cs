656a7338b7db102d71338a62ebe20ff62dc21c4a
RoachieBoy <64902177+RoachieBoy@users.noreply.github.com>
2023-10-25 14:43:23 +0200

    Changelog
    - Started working on new audio visuals
    --> completed simple line renderer effect
    - Polished up design a bit more

---
+++ MAGI/Assets/Scripts/Visual Effects/Audio Visuals New/BandTypes.cs
@@ -0,0 +1,12 @@
+﻿namespace Visual_Effects.Audio_Visuals_New
+{
+    /// <summary>
+    ///   The different types of bands that can be used for the frequency analyser.
+    ///   Bands are the number of frequency bands that are used to analyse the audio.
+    /// </summary>
+    public enum BandTypes
+    {
+        Eight = 8,
+        SixtyFour = 64,
+    }
+}
---
+++ MAGI/Assets/Scripts/Visual Effects/Audio Visuals New/FrequencyAnalyser.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using UnityEngine;
+
+namespace Visual_Effects.Audio_Visuals_New
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class FrequencyAnalyser: MonoBehaviour
+    {
+        // represents the number of samples per second
+        private const int FrequencyBins64 = 64;
+        private const int FrequencyBins8 = 8;
+        private const int BinAmounts = 512;
+        
+        private float[] _samples;
+        private float[] _sampleBuffer;
+        private float [] _freqBands8;
+        private float [] _freqBands64;
+
+        private readonly int[] _powerValues = {16, 32, 40, 48, 56};
+        
+        private AudioSource _audioSource;
+
+        [Header("Frequency Band Settings")] 
+        [SerializeField] private float smoothDownRate = 1;
+        [SerializeField] private  float scalar = 1;
+        [SerializeField] private FFTWindow fftWindow = FFTWindow.BlackmanHarris;
+        
+
+        private void Start()
+        {
+            _audioSource = gameObject.GetComponent<AudioSource>();
+            
+            // initialise arrays for storing frequency band data
+            _freqBands8 = new float[FrequencyBins8];
+            _freqBands64 = new float[FrequencyBins64];
+            
+            // initialise arrays for storing audio samples and sample buffer
+            _samples = new float[BinAmounts];
+            _sampleBuffer = new float[BinAmounts];
+        }
+        
+        private void FixedUpdate()
+        {
+            _audioSource.GetSpectrumData(_samples, 0, fftWindow);
+
+            for (var i = 0;  i < _samples.Length; i++)
+            {
+                // if the current sample is greater than the sample buffer, set the sample buffer to the current sample
+                if (_sampleBuffer[i] < _samples[i]) _sampleBuffer[i] = _samples[i];
+
+                else _sampleBuffer[i] -= Mathf.Lerp(_samples[i], _sampleBuffer[i], Time.deltaTime * smoothDownRate);
+            }
+            
+            UpdateFreqBands8();
+            UpdateFreqBands64();
+        }
+
+        /// <summary>
+        ///  Updates the 64 frequency bands for visualisation
+        /// </summary>
+        private void UpdateFreqBands64()
+        {
+            var count = 0;
+            var power = 0;
+            var sampleCount = 1; 
+
+            for (var i = 0; i < FrequencyBins64; i++)
+            {
+                var average = 0f;
+
+                foreach (var value in _powerValues)
+                {
+                    if (i != value) continue;
+                    
+                    power++;
+                    sampleCount = (int)Mathf.Pow(2, power);
+                    
+                    if (power == 3) sampleCount -= 2;
+                }
+                
+                for (var j = 0; j < sampleCount; j++)
+                {
+                    average += _samples[count] * (count + 1);
+                    count++;
+                }
+                
+                average /= count;
+                
+                _freqBands64[i] = average * scalar;
+            }
+        }
+
+        /// <summary>
+        ///  Updates the 8 frequency bands for visualisation
+        /// </summary>
+        private void UpdateFreqBands8()
+        {
+            var count = 0;
+            
+            for (var i = 0; i < FrequencyBins8; i++)
+            {
+                var average = 0f;
+                var sampleCount = (int)Mathf.Pow(2, i) * 2;
+                
+                // When we reach the last frequency band, we need to add 2 samples to the sample count
+                // this is because the last frequency band is 22000 - 22050 hz which is outside the
+                // range of human hearing
+                if (i == FrequencyBins8 - 1) sampleCount += 2;
+                
+                for (var j = 0; j < sampleCount; j++)
+                {
+                    average += _samples[count] * (count + 1);
+                    count++;
+                }
+                
+                average /= count;
+                
+                _freqBands8[i] = average * scalar;
+            }
+        }
+        
+        /// <summary>
+        ///  Returns the amount of a given frequency band
+        /// </summary>
+        /// <param name="bands"> which band type is being used </param>
+        /// <param name="index"> what is the current index </param>
+        /// <returns></returns>
+        public float GetBandAmount(BandTypes bands, int index)
+        {
+            return bands switch
+            {
+                BandTypes.Eight => _freqBands8[index],
+                BandTypes.SixtyFour => _freqBands64[index],
+                _ => 0
+            };
+        }
+    }
+}

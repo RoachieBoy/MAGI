ff8d9cffd1afd7287d49c4e0c248e3a75c080b46
RoachieBoy <64902177+RoachieBoy@users.noreply.github.com>
2023-11-04 15:57:22 +0100

    clamped line height vs effect

--- MAGI/Assets/Scripts/Visual Effects/Audio Visuals/Audio Visual Effects/RetroAudioBars.cs
+++ MAGI/Assets/Scripts/Visual Effects/Audio Visuals/Audio Visual Effects/RetroAudioBars.cs
@@ -1,44 +1,51 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Visual_Effects.Audio_Visuals.Audio_Visual_Effects
 {
     public class RetroAudioBars : AudioVisualEffect
     {
-        [Header("Prefab and Bars")]
-        [SerializeField] private GameObject barPrefab;
+        [Header("Prefab and Bars")] [SerializeField]
+        private GameObject barPrefab;
+
         [Range(1, 50)] [SerializeField] private int numberOfBars = 10;
-        [SerializeField] private float spacing = 1.0f;
-        [Range(1, 10)] [SerializeField] private float growSpeed = 2.0f;
-        [Range(1, 10)] [SerializeField] private float shrinkSpeed = 2.0f;
+
+        [Header("Bar Settings")] [Range(0.1f, 1)] [SerializeField]
+        private float spacing = 1.0f;
+
+        [Range(1, 10)] [SerializeField] private float barSpeed = 2.0f;
+        [Range(1, 10)] [SerializeField] private float scaleFactor = 5.0f;
+        [Range(1, 10)] [SerializeField] private float maxHeight = 1.0f;
+        [Range(0.1f, 1)] [SerializeField] private float minHeight = 0.1f;
 
         private readonly List<GameObject> _bars = new();
         private Vector3 _initialPosition;
 
         private void Start()
         {
-            CreateBars();
-        }
-
-        private void CreateBars()
-        {
             _initialPosition = transform.position;
 
             for (int i = 0; i < numberOfBars; i++)
             {
                 var bar = Instantiate(barPrefab, transform);
+
                 bar.transform.position = new Vector3(_initialPosition.x + (spacing * i),
                     _initialPosition.y, _initialPosition.z);
+
                 _bars.Add(bar);
             }
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
             for (int i = 0; i < _bars.Count; i++)
             {
                 var bar = _bars[i];
-                var yPos = FrequencyAnalyser.GetBandAmount(FrequencyBandCount, i) * 10; // Adjust the scaling factor as needed
+                var yPos = FrequencyAnalyser.GetBandAmount(FrequencyBandCount, i) * scaleFactor;
+
+                // Clamp the yPos value to stay within a maximum value
+                yPos = Mathf.Clamp(yPos, minHeight, maxHeight);
 
                 // Calculate the bottom position of the bar
                 var bottomPosition = bar.transform.position - Vector3.up * bar.transform.localScale.y / 2;
@@ -47,8 +54,8 @@ namespace Visual_Effects.Audio_Visuals.Audio_Visual_Effects
                 var currentScale = bar.transform.localScale;
                 var targetScale = new Vector3(currentScale.x, yPos, currentScale.z);
 
-                // Ensure the bars only grow from the bottom
-                currentScale = Vector3.Lerp(currentScale, targetScale, Time.deltaTime * growSpeed);
+                // Ensure the bars only grow from the bottom and shrink from the top
+                currentScale = Vector3.Lerp(currentScale, targetScale, Time.deltaTime * barSpeed);
 
                 // Update the position to keep the top of the bar fixed
                 bar.transform.position = bottomPosition + Vector3.up * currentScale.y / 2;
@@ -56,21 +63,22 @@ namespace Visual_Effects.Audio_Visuals.Audio_Visual_Effects
             }
         }
 
-
         public override void DisableEffect()
         {
             gameObject.SetActive(false);
-
             BarState(false);
         }
 
         public override void EnableEffect()
         {
             gameObject.SetActive(true);
-
             BarState(true);
         }
 
+        /// <summary>
+        ///   Sets the state of the bars
+        /// </summary>
+        /// <param name="state"> the state of the bar </param>
         private void BarState(bool state)
         {
             foreach (var bar in _bars)
--- MAGI/Assets/Scripts/Visual Effects/Audio Visuals/Audio Visual Effects/SpectrumLine.cs
+++ MAGI/Assets/Scripts/Visual Effects/Audio Visuals/Audio Visual Effects/SpectrumLine.cs
@@ -3,32 +3,31 @@
 namespace Visual_Effects.Audio_Visuals.Audio_Visual_Effects
 {
     [RequireComponent(typeof(LineRenderer))]
-    public class SpectrumLine: AudioVisualEffect
+    public class SpectrumLine : AudioVisualEffect
     {
         private const float MinimumYPosition = 0.01f;
-        
-        [Header("Line Settings")] 
-        [Range(1f, 25f)] 
-        [SerializeField] private float lineLength = 2;
 
-        [Range(1f, 20f)] 
-        [SerializeField] private float strength = 1;
+        [Header("Line Settings")] [Range(1f, 25f)] [SerializeField]
+        private float lineLength = 2;
 
-        [Range(0, 20f)] 
-        [SerializeField] private float smoothRate = 1;
-        
+        [Range(1f, 20f)] [SerializeField] private float strength = 1;
+
+        [Range(0, 20f)] [SerializeField] private float smoothRate = 1;
+
+        [Range(1f, 10f)] [SerializeField] private float maxLineLengthHeight = 1;
+
         private LineRenderer _line;
         private float _spacing;
 
         private void Start()
         {
             _line = gameObject.GetComponent<LineRenderer>();
-            
+
             // set the amount of points on the line to the amount of frequency bands 
             _line.positionCount = FrequencyBandCount == BandTypes.Eight
                 ? (int) BandTypes.Eight
                 : (int) BandTypes.SixtyFour;
-            
+
             // set the spacing between the points on the line
             _spacing = lineLength / _line.positionCount;
         }
@@ -41,7 +40,10 @@ namespace Visual_Effects.Audio_Visuals.Audio_Visual_Effects
 
                 // Get the current y position from the audio data and scale it
                 var yPos = FrequencyAnalyser.GetBandAmount(FrequencyBandCount, i) * strength;
-                
+
+                // clamp the yPos value to stay within a maximum value
+                yPos = Mathf.Clamp(yPos, MinimumYPosition, maxLineLengthHeight);
+
                 var newYPos = Mathf.Lerp(_line.GetPosition(i).y, yPos, Time.deltaTime * smoothRate);
 
                 // If there's no data, smoothly interpolate the y position to 0
